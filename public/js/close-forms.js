6cf85d1d0602b56555b776f400862a7344b87740
Chris Talley <christopherr.talley@gmail.com>
2020-04-27 03:30:11 -0500

    Forms done and styled

--- public/js/close-forms.js
+++ public/js/close-forms.js
@@ -1,11 +1,22 @@
-const closeForm = document.querySelectorAll(".close-out-form");
+const closeForms = document.querySelectorAll(".close-out-form");
+const weirdForms = document.querySelectorAll(".weird-form");
 
-
-// Each close out form icon has an id of the form
+// Each close out form icon has an id that macthes the class of the form
 // That it is on. When a user clicks on it, the interpolated version
-// of e.target.id is used toggle the desired form close because the id matches
+// of e.target.id is used to toggle the desired form close because the id matches
 
-closeForm.forEach((button) => {
+closeForms.forEach((button) => {
+    button.addEventListener("click", async (e) => {
+        console.log(e.target.id)
+        let desiredForm = document.querySelector(`.${e.target.id}`);
+        desiredForm.classList.toggle("hidden");
+    })
+});
+
+// WEIRD FORMS  ARE THE ONES WITH JUST CONFIRM OR DENY AND
+// had to be styled differently
+
+weirdForms.forEach((button) => {
     button.addEventListener("click", async (e) => {
         console.log(e.target.id)
         let desiredForm = document.querySelector(`.${e.target.id}`);
