25c3ee6040eabba8cd1817aaa8c8a246299982f2
Chris Talley <christopherr.talley@gmail.com>
2020-04-28 09:52:34 -0500

    FInal fixes

--- public/js/join-server.js
+++ public/js/join-server.js
@@ -65,7 +65,7 @@ joinButton.addEventListener('click', async (e) => {
     newServer.dataset.serverName = server.serverName;
     serverTitle.innerHTML = server.serverName;
     const randomImg = getRandomImg;
-    newServer.innerHTML = `<h3 class="NameDisplay${joinServerId} server-names hidden">${server.serverName} </h3><img src="${randomImg}" class="server-display">`;
+    newServer.innerHTML = `<h3 class="NameDisplay${joinServerId} server-names hidden">${server.serverName} </h3><img src="${randomI" class="server - display">`;
 
     newServer.addEventListener('mouseenter', async (e) => {
         console.log("in");
