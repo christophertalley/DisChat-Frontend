f0d77684f7d733d843469ffdfb7549b40a6e8560
Chris Talley <christopherr.talley@gmail.com>
2020-07-22 16:41:51 -0500

    Worked on about page

--- public/js/app-info.js
+++ public/js/app-info.js
@@ -16,3 +16,17 @@ infoButton.addEventListener("click", async (e) => {
         formChannel.classList.add('hidden')
     }
 });
+const infoIcon = document.querySelector('.fa-info');
+    infoIcon.addEventListener("click", async(e)=>{
+        appInfo.classList.toggle("hidden");
+
+        if (!searchForm.classList.contains('hidden')) {
+            searchForm.classList.add('hidden')
+        }
+        if (!formServer.classList.contains('hidden')) {
+            leaveServer.classList.add('hidden')
+        }
+        if (!formChannel.classList.contains('hidden')) {
+            formChannel.classList.add('hidden')
+        }
+})
